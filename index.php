16851d4137bfd388322266d98eb78d3d9a5d7fb8
souhila <souhilaatr@gmail.com>
2019-03-22 12:15:35 +0100

    add an index to the ionic project

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=abonnes','root','',array(
+
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+));
+
+if(isset($_POST["email"])){
+    $login = $_POST["email"];
+    $psd = $_POST["psd"];
+    $result = $pdo->prepare("SELECT id_identification From identification WHERE email=? AND  psd =?");
+    $result->execute(array($login,  $psd));
+    
+     $valeur = $result -> fetch();
+    // echo "mail :". $valeur['email'].'</br>';
+     //echo "psd :". $valeur['psd'].'</br>';
+    // echo "le nombre de resultat possible". $result -> rowCount().'</br>';
+     $id_identification = $valeur["id_identification"];
+     if($result->rowCount() >= 1){
+         //echo "connexion reussi";
+        
+         header("Location: article.php?id_identification=".$id_identification);
+         exit();
+     }
+      
+     
+    //$pdo -> query("INSERT INTO  rubrique(culture_pop,sport, cuisine, actualite, mode,tuning) values ()"); 
+    
+   
+   /*
+    if ($compare["email"] ==$login AND $compare["psd"] == $psd){
+
+                echo "bonjour";
+    }else{
+        echo "erreur";
+    }*/
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Page index  to see it in a GIT </h1>
+
+
+    <form action="index.php" method="post">
+
+        <input type="text" name="email"> 
+         <input type="text" name="psd"> 
+      
+        <button type="submit" name="valider">valider</button>
+    
+    </form>
+</body>
+</html>
